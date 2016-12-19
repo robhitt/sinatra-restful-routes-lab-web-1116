5c2e2159b8b06c00f5282520cceab7d4203a5e40
Rob Hitt <robhitt@gmail.com>
2016-12-19 17:06:34 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,50 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    redirect :recipes
+  end
+
+  get '/recipes' do
+    @all_recipes = Recipe.all
     erb :index
   end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params[:recipe])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect '/recipes'
+  end
 end
+
+
+# In the application_controller.rb, set up a controller action that will render a form to create a new recipe. This controller action should create and save this new recipe to the database.
+#
+# Again in the application_controller.rb, create a controller action that uses RESTful routes to display a single recipe.
